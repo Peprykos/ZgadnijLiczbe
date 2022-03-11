35b04d5a12d2f20aeda28ee8c8e91b4ab84e0164
3109762 <3109762@TE-GDA-D-D26F13>
2022-03-11 15:06:10 +0100

    Dodaj pliki projektów.

---
+++ ZgadnijLiczbe/Program.cs
@@ -0,0 +1,36 @@
+﻿Random c = new Random();
+int liczba1 = c.Next(1, 10);
+int y = 0;
+
+Console.WriteLine("Licba została wylosowana");
+while (true)
+{
+    string wybor = Console.ReadLine();
+
+    object int32 = null;
+    if (!Int32.TryParse(wybor, out y))
+    {
+        Console.WriteLine("Zgadnij liczbę: ");
+        continue;
+    }
+
+    if (y == liczba1)
+    {
+        Console.WriteLine("Wygrałeś");
+        break;
+    }
+    else if (y > liczba1)
+    {
+        Console.WriteLine("Liczba jest mniejsza.");
+        continue;
+    }
+    else if (y < liczba1)
+    {
+        Console.WriteLine("Liczba jest większa.");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Przegrałes.");
+    }
+}
